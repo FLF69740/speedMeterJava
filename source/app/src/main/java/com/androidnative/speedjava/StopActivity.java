0a7789b5ede3303853a49ea0722470194009f22e
slaye <lefort.francois_pro@yahoo.com>
2020-05-21 11:58:20 +0200

    STOP ACTIVITY - creation of StopActivity

---
+++ source/app/src/main/java/com/androidnative/speedjava/StopActivity.java
@@ -0,0 +1,17 @@
+package com.androidnative.speedjava;
+
+public class StopActivity extends BaseActivity {
+
+    @Override
+    public int getLayout() { return R.layout.activity_stop; }
+
+    @Override
+    public void bodyActivity() {
+
+    }
+
+    @Override
+    public void speedAction(short result) {
+
+    }
+}
